0705f5ab51f320ff45d17c7f441ef102ffd98ec3
Alexander <48926283+canek13@users.noreply.github.com>
2020-05-30 21:04:06 +0300

    updated runs

---
+++ run/run2.sh
@@ -0,0 +1,31 @@
+SOURCE_BASE_PATH="$HOME/hadoop"
+echo $SOURCE_BASE_PATH
+
+INPUT_DIR="/hadoop/input"
+OUTPUT_DIR="/hadoop/output"
+
+HADOOP_STREAMING_PATH="/usr/lib/hadoop-mapreduce/hadoop-streaming.jar"
+
+hdfs dfs -rm -r $INPUT_DIR
+hdfs dfs -rm -r $OUTPUT_DIR
+
+hdfs dfs -mkdir -p $INPUT_DIR
+hdfs dfs -copyFromLocal $SOURCE_BASE_PATH/output1  $INPUT_DIR
+
+chmod 0777 $SOURCE_BASE_PATH/src/similarity_mapper2.py
+chmod 0777 $SOURCE_BASE_PATH/src/similarity_reducer2.py
+
+hadoop_streaming_arguments="\
+		-D mapreduce.job.reduces=12 \
+		-file $SOURCE_BASE_PATH/src/similarity_mapper2.py -mapper similarity_mapper2.py  \
+		-file $SOURCE_BASE_PATH/src/similarity_reducer2.py -reducer similarity_reducer2.py \
+		-file $SOURCE_BASE_PATH/data/ratings.csv \
+		-input $INPUT_DIR/* -output $OUTPUT_DIR \
+		"
+
+hadoop jar $HADOOP_STREAMING_PATH $hadoop_streaming_arguments
+
+hdfs dfs -copyToLocal $OUTPUT_DIR $SOURCE_BASE_PATH/output2
+
+hdfs dfs -rm -r $INPUT_DIR
+hdfs dfs -rm -r $OUTPUT_DIR
---
+++ run/run5.sh
@@ -0,0 +1,34 @@
+SOURCE_BASE_PATH="$HOME/hadoop"
+echo $SOURCE_BASE_PATH
+
+cp $SOURCE_BASE_PATH/data/movies.csv $SOURCE_BASE_PATH/output4
+echo copied
+
+
+INPUT_DIR="/hadoop/input"
+OUTPUT_DIR="/hadoop/output"
+
+HADOOP_STREAMING_PATH="/usr/lib/hadoop-mapreduce/hadoop-streaming.jar"
+
+hdfs dfs -rm -r $INPUT_DIR
+hdfs dfs -rm -r $OUTPUT_DIR
+
+hdfs dfs -mkdir -p $INPUT_DIR
+hdfs dfs -copyFromLocal $SOURCE_BASE_PATH/output4  $INPUT_DIR
+
+chmod 0777 $SOURCE_BASE_PATH/src/name_films_mapper5.py
+chmod 0777 $SOURCE_BASE_PATH/src/name_films_reducer5.py
+
+hadoop_streaming_arguments="\
+		-D mapreduce.job.reduces=6 \
+		-file $SOURCE_BASE_PATH/src/name_films_mapper5.py -mapper name_films_mapper5.py  \
+		-file $SOURCE_BASE_PATH/src/name_films_reducer5.py -reducer name_films_reducer5.py \
+		-input $INPUT_DIR/* -output $OUTPUT_DIR \
+		"
+
+hadoop jar $HADOOP_STREAMING_PATH $hadoop_streaming_arguments
+
+hdfs dfs -copyToLocal $OUTPUT_DIR $SOURCE_BASE_PATH/output5
+
+hdfs dfs -rm -r $INPUT_DIR
+hdfs dfs -rm -r $OUTPUT_DIR
